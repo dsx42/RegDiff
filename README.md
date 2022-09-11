fb92bde3cd8c2d83d3402d9a11c2f544ca690cf9
dsx42 <sizhongwork@foxmail.com>
2022-09-11 16:28:51 +0800

    第一次提交

--- README.md
+++ README.md
@@ -1,2 +1,14 @@
-# RegDiff
-找出 Windows 系统设置修改对应的注册表变化
+# RegDiff 简介
+
+* 找出 Windows 系统设置修改对应的注册表变化
+* 需要搭配文本比较软件一起使用，如 WinMerge：https://winmerge.org/downloads
+
+# 下载
+
+从如下链接下载最新的版本：`RegDiff_版本号.zip`
+
+> https://github.com/dsx42/RegDiff/releases
+
+# 如何使用本工具？
+
+解压下载好的压缩包，双击解压后的 `RegDiff.cmd` 即可
---
+++ RegDiff.ps1
@@ -0,0 +1,47 @@
+﻿function SaveReg {
+    param($Path)
+
+    Write-Host -Object ''
+    Write-Host -Object '正在保存设置修改前的注册表数据' -ForegroundColor Yellow
+
+    if (!(Test-Path -Path "$Path\old" -PathType Container)) {
+        New-Item -Path "$Path\old" -ItemType Directory -Force | Out-Null
+    }
+    if (!(Test-Path -Path "$Path\new" -PathType Container)) {
+        New-Item -Path "$Path\new" -ItemType Directory -Force | Out-Null
+    }
+
+    reg export HKLM "$Path\old\hklm.txt" /y
+    reg export HKCU "$Path\old\hkcu.txt" /y
+    reg export HKCR "$Path\old\hkcr.txt" /y
+    reg export HKU "$Path\old\hku.txt" /y
+    reg export HKCC "$Path\old\hkcc.txt" /y
+
+    Write-Host -Object ''
+    Write-Host -Object '设置修改前的注册表数据保存成功' -ForegroundColor Green
+
+    Write-Host -Object ''
+    Read-Host -Prompt '设置修改后请按回车键'
+
+    Write-Host -Object ''
+    Write-Host -Object '正在保存设置修改后的注册表数据' -ForegroundColor Yellow
+
+    reg export HKLM "$Path\new\hklm.txt" /y
+    reg export HKCU "$Path\new\hkcu.txt" /y
+    reg export HKCR "$Path\new\hkcr.txt" /y
+    reg export HKU "$Path\new\hku.txt" /y
+    reg export HKCC "$Path\new\hkcc.txt" /y
+
+    Write-Host -Object ''
+    Write-Host -Object '设置修改后的注册表数据保存成功' -ForegroundColor Green
+}
+
+while ($true) {
+    Write-Host -Object ''
+    $Desc = Read-Host -Prompt '请输入要修改的设置的描述'
+    if (!$Desc) {
+        continue
+    }
+
+    SaveReg -Path "$PSScriptRoot\RegData\$Desc"
+}
